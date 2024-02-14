cbcec2bc4c4a9e50d543f53f4db4a77a676c1731
kimeomji333 <148833676+kimeomji333@users.noreply.github.com>
2024-02-14 23:35:01 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -2,12 +2,12 @@ class Solution {
     public boolean solution(int x) {
         int sum = 0;
         int n = x;
-        
-        while (n >= 1) {
+
+        while (n > 0) {
             sum += n % 10;
             n /= 10;
         }
-        if ( x % sum == 0) {
+        if (x % sum == 0) {
             return true;
         } else {
             return false;
