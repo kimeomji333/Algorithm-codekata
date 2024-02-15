61912d29da36d3ec4a57da7edb2540885eab37be
kimeomji333 <148833676+kimeomji333@users.noreply.github.com>
2024-02-15 14:58:46 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,10 +1,7 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int sum = 0;
-
-        // 배열의 길이 같다.
         for (int i = 0; i < absolutes.length; i++) {
-
             if (signs[i] == true) {
                 sum += absolutes[i];
             } else if (signs[i] == false) {
