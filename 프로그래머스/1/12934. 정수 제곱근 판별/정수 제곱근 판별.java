323e9e2c7d87ef97543fd29b636764584881c429
kimeomji333 <148833676+kimeomji333@users.noreply.github.com>
2024-03-05 22:58:35 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,11 +1,13 @@
-public class Solution {
+class Solution {
     public long solution(long n) {
-         long x = (long) Math.sqrt(n);
-        if (n == Math.pow(x,2)) {
-            return (long) Math.pow(x+1,2);
-        } else {
-            return -1;
-        }
-    }
+        long answer = 0;
+        long x = (long)Math.sqrt(n);
 
+        if (n == Math.pow(x, 2)) {
+            answer = (long) Math.pow((x+1),2);
+        } else {
+            answer = -1;
+        }
+        return answer;
+    }
 }
