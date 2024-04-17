69db8568d4a1fe1afe88acbc34bc8fba2c99d751
kimeomji333 <148833676+kimeomji333@users.noreply.github.com>
2024-04-17 09:43:45 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,14 +1,11 @@
 class Solution {
     public String solution(String my_string) {
-        String answer ="";
-        char[] chars = my_string.toCharArray();
-        char[] reverse = new char[chars.length];
         
-        for(int i = 0; i < chars.length; i++) {
-            reverse[i] = chars[chars.length -i -1];
+        StringBuilder reversed = new StringBuilder();
+        
+        for(int i = my_string.length() - 1; i >= 0; i--) {
+            reversed.append(my_string.charAt(i));
         }
-        
-        answer = new String(reverse);
-        return answer;
+        return reversed.toString();
     }
 }
