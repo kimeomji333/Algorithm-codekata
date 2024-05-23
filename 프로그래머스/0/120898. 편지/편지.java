84e3666b693ed140a3010f204a7849ee4700c954
김엄지 <148833676+kimeomji333@users.noreply.github.com>
2024-05-23 22:49:09 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String message) {
+        int length = message.length();
+        int requiredLength = length * 2;
+        
+        return requiredLength;
+    }
+}
